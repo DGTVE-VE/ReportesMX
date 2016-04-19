a2f9cca810b6a30868837488e6fe976a9dae93bd
jorgeomarmh <jorgeomarmh@gmail.com>
2016-04-19 17:38:49 -0500

    Nombre cursos

--- resources/views/cursoc.blade.php
+++ resources/views/cursoc.blade.php
@@ -1,5 +1,5 @@
 @extends('cursos') @section('contentd')
-<center> <h4>Hola, {{$name_user}}, {{$course_name}}</h4></center>
+<center> <h4>Información de "{{$course_name}}"</h4></center>
 <div class="container">
     <div class="row">
         <div>
