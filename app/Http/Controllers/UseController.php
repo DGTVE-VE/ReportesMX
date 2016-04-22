efd58a498ac040e80da0d4af0f16b90063a9f506
jorgeomarmh <jorgeomarmh@gmail.com>
2016-04-22 12:32:06 -0500

    envio de datos

--- app/Http/Controllers/UseController.php
+++ app/Http/Controllers/UseController.php
@@ -709,7 +709,7 @@ class UseController extends Controller {
 			}
 			print_r($inscrito_curso);
 			// print_r($lista_constancias);
-			return view('usuarios/inscritost')-> with('mes1', collect($mes))-> with('mes2', collect($cur))-> with('name_user', $username)->with('users_course', collect($users_course))->with('constancias', $constancias)->with('lista_constancias', $lista_constancias);
+			return view('usuarios/inscritost')-> with('mes1', collect($mes))-> with('mes2', collect($cur))-> with('name_user', $username)->with('users_course', collect($users_course))->with('constancias', $constancias)->with('lista_constancias', $lista_constancias)->with('inscrito_curso', $inscrito_curso);
 
 		}
 		else
