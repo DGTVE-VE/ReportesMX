0a93665b7af30238e5ce391f4137d1654909e913
jorgeomarmh <jorgeomarmh@gmail.com>
2016-04-22 11:40:58 -0500

    no collect

--- app/Http/Controllers/UseController.php
+++ app/Http/Controllers/UseController.php
@@ -701,7 +701,7 @@ class UseController extends Controller {
 			$constancias = DB::table('edxapp.constancias')->count('id');
 			$lista_constancias = DB::select(DB::raw('select count(curso) as constancias , curso as nombre_curso from edxapp.constancias group by curso'));
 
-			return view('usuarios/inscritost')-> with('mes1', collect($mes))-> with('mes2', collect($cur))-> with('name_user', $username)->with('users_course', collect($users_course))->with('constancias', $constancias)->with('lista_constancias', collect($lista_constancias));
+			return view('usuarios/inscritost')-> with('mes1', collect($mes))-> with('mes2', collect($cur))-> with('name_user', $username)->with('users_course', collect($users_course))->with('constancias', $constancias)->with('lista_constancias', $lista_constancias);
 
 		}
 		else
