7671febacbc1ef460e6e28aaf9a71f59033f1961
Israel Toledo <j.israel.toledo@gmail.com>
2016-05-19 12:37:57 -0500

    Se creó el template del envío masivo

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class MailController extends Controller {
+
+    public function sendmail() {
+        Mail::send(
+                'emails.masivo', 
+                array('firstName' => 'Israel'), 
+                function( $message ) {
+                    $message->from('mexicox@televisioneducativa.gob.mx', 'México X');
+                    $message->to('j.israel.toledo@gmail.com')
+                            ->subject('Información México X');
+                }
+        );
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id) {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+        //
+    }
+
+}
---
+++ resources/views/emails/masivo.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title> México X</title>
+    </head>
+    <body>
+        <h1></h1>
+        <img src='{{asset ('imagenes/email/header.jpg')}}'>
+    </body>
+</html>
