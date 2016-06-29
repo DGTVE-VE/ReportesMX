065fdc733501363e29ad1fdc371883e5b2e0d4f5
jorgeomarmh <jorgeomarmh@gmail.com>
2016-06-29 14:00:36 -0500

    prueba 6

--- app/Http/Controllers/MXController.php
+++ app/Http/Controllers/MXController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use DB;
 use Illuminate\Http\Request;
 
-class UseController extends Controller {
+class MXController extends Controller {
 
 	public function __construct()
 	{
