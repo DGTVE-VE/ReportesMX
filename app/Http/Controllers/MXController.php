6d7f98816b02d6f2fbd884edf9dbcffd1e73417e
Israel Toledo <j.israel.toledo@gmail.com>
2016-07-21 13:45:16 -0500

    Probando el upload del video

--- app/Http/Controllers/MXController.php
+++ app/Http/Controllers/MXController.php
@@ -1,142 +1,133 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use DB;
 use File;
 use Illuminate\Http\Request;
 
 class MXController extends Controller {
 
-	public function __construct()
-	{
-	}
+    public function __construct() {
+        
+    }
 
-	public function validarcp(){
+    public function validarcp() {
 
-		$codigo = filter_input(INPUT_GET, 'codigopostal');
+        $codigo = filter_input(INPUT_GET, 'codigopostal');
 
-		$direccion = DB::table('codigospostales')->wherecodigopostal($codigo)->get();
+        $direccion = DB::table('codigospostales')->wherecodigopostal($codigo)->get();
 
-		if(empty($direccion)){
-			echo $_GET['callback']."(".json_encode('0').")";
+        if (empty($direccion)) {
+            echo $_GET['callback'] . "(" . json_encode('0') . ")";
+        } else {
+            echo $_GET['callback'] . "(" . json_encode($direccion) . ")";
+        }
+    }
 
-		}else {
-			echo $_GET['callback']."(".json_encode($direccion).")";
-		}
+    public function verifica() {
 
-	}
+        $id_usuario = filter_input(INPUT_GET, 'id');
 
-	public function verifica(){
+        $city = DB::table('auth_userprofile')->whereuser_id($id_usuario)->get();
+        //$city1 = DB::table('users_info')->whereusers_id($id_usuario)->get();
 
-		$id_usuario = filter_input(INPUT_GET, 'id');
-
-		$city = DB::table('auth_userprofile')->whereuser_id($id_usuario)->get();
-		//$city1 = DB::table('users_info')->whereusers_id($id_usuario)->get();
-
-		$country = DB::table('auth_userprofile')->whereuser_id($id_usuario)->get();
-		//$country1 = DB::table('users_info')->whereusers_id($id_usuario)->get();
+        $country = DB::table('auth_userprofile')->whereuser_id($id_usuario)->get();
+        //$country1 = DB::table('users_info')->whereusers_id($id_usuario)->get();
 
 
-		if( empty($country[0]->country) || ( $country[0]->country == NULL ) ){
+        if (empty($country[0]->country) || ( $country[0]->country == NULL )) {
 
-			//falta pais
-			echo $_GET['callback']."(".json_encode('0').")";
+            //falta pais
+            echo $_GET['callback'] . "(" . json_encode('0') . ")";
+        } else if (( isset($country[0]->country) ) && ( ( empty($city[0]->city) ) || ( $city[0]->city == NULL ) )) {
 
-		}else if ( ( isset($country[0]->country)  ) && ( ( empty($city[0]->city) ) || ( $city[0]->city == NULL ) ) ) {
+            echo $_GET['callback'] . "(" . json_encode($country[0]->country) . ")";
+        } else {
+            //no falta nada
+            echo $_GET['callback'] . "(" . json_encode('1') . ")";
+        }
+    }
 
-			echo $_GET['callback']."(".json_encode($country[0]->country).")";
+    public function adddata() {
 
-		}else{
-			//no falta nada
-			echo $_GET['callback']."(".json_encode('1').")";
-		}
+        $id_usuario = filter_input(INPUT_GET, 'id');
+        $pais = filter_input(INPUT_GET, 'country');
+        $estado = filter_input(INPUT_GET, 'state');
+        $cp = filter_input(INPUT_GET, 'codigopostal');
 
-	}
+        if (empty($pais)) {
+            echo $_GET['callback'] . "(" . json_encode('Error country void') . ")";
+            return 0;
+        } else if (empty($estado)) {
+            echo $_GET['callback'] . "(" . json_encode('Error state void') . ")";
+            return 0;
+        } else if (empty($cp)) {
+            echo $_GET['callback'] . "(" . json_encode('Error cp void') . ")";
+            return 0;
+        } else {
 
-	public function adddata(){
+            if ($pais == "MX") {
+                $codigo = DB::table('codigospostales')->wherecodigopostal($cp)->get();
+                if (empty($codigo)) {
+                    echo $_GET['callback'] . "(" . json_encode('Error invalid cp') . ")";
+                    return 0;
+                }
+            }
 
-		$id_usuario = filter_input (INPUT_GET, 'id');
-		$pais = filter_input (INPUT_GET, 'country');
-		$estado = filter_input (INPUT_GET, 'state');
-		$cp = filter_input (INPUT_GET, 'codigopostal');
+            $exito = DB::table('auth_userprofile')->where('user_id', $id_usuario)->update(['country' => $pais, 'city' => $estado, 'mailing_address' => $cp]);
 
-		if(empty($pais)){
-			echo $_GET['callback']."(".json_encode('Error country void').")";
-			return 0;
-		}
-		else if(empty($estado)){
-			echo $_GET['callback']."(".json_encode('Error state void').")";
-			return 0;
-		}
-		else if(empty($cp)){
-			echo $_GET['callback']."(".json_encode('Error cp void').")";
-			return 0;
-		}else {
+            if ($exito == 1) {
+                print_r("Exito Update");
+                echo $_GET['callback'] . "(" . json_encode('Success Update') . ")";
+                return 1;
+            } else {
+                print_r("Error Update");
+                echo $_GET['callback'] . "(" . json_encode('Error Update') . ")";
+                return 0;
+            }
+        }
+    }
 
-			if($pais == "MX"){
-				$codigo = DB::table('codigospostales')->wherecodigopostal($cp)->get();
-				if(empty($codigo)){
-					echo $_GET['callback']."(".json_encode('Error invalid cp').")";
-					return 0;
-				}
-			}
+    public function uploadvideo() {
 
-			$exito = DB::table('auth_userprofile')->where('user_id', $id_usuario)->update(['country' => $pais, 'city' => $estado, 'mailing_address' => $cp]);
+        return view('upload.uploadvideo');
+    }
 
-			if($exito == 1){
-				print_r("Exito Update");
-				echo $_GET['callback']."(".json_encode('Success Update').")";
-				return 1;
-			}else {
-				print_r("Error Update");
-				echo $_GET['callback']."(".json_encode('Error Update').")";
-				return 0;
-			}
+    public function savevideo(Request $request) {
 
-	}
-}
+        $this->validate($request, [
 
-public function uploadvideo(){
+            'inputEmai' => 'required|email|max:150',
+            'inputText' => 'required|string|max:1000',
+            'inputVideo' => 'required|max:20000',
+        ]);
 
-	return view('upload.uploadvideo');
-}
+        $video = $request->file('inputVideo');
+        $texto = $request->input('inputText');
+        $email = $request->input('inputEmai');
 
-public function savevideo(Request $request){
+        $id = DB::table('upload_video')->max('id');
 
-	$this->validate($request, [
+        \Storage::MakeDirectory($id);
 
-		'inputEmai'  => 'required|email|max:150',
-		'inputText'  => 'required|string|max:1000',
-		'inputVideo'  => 'required|max:20000',
+        $vid = $video->getClientOriginalName();
 
-	]);
+        $route1 = \Storage::disk('local')->put($id . '/' . $vid, File::get($video));
 
-	$video = $request->file('inputVideo');
-	$texto = $request->input('inputText');
-	$email = $request->input('inputEmai');
+        $exito = DB::table('upload_video')->insert(
+                [
+                    'correo' => $email,
+                    'texto' => $texto,
+                    'ruta_video' => '/public/uploadvideos/' . $id . '',
+                ]
+        );
 
-	$id = DB::table('upload_video')->max('id');
-
-	\Storage::MakeDirectory($id);
-
-	$vid = $video->getClientOriginalName();
-
-	$route1 = \Storage::disk('local')->put($id.'/'.$vid, File::get($video));
-
-		$exito = DB::table('upload_video')->insert(
-		[
-			'correo' => $email,
-			'texto' => $texto,
-			'ruta_video' => '/public/uploadvideos/'.$id.'',
-			]
-		);
-
-		if($exito == 1 ){
-			return "Gracias";
-		}else {
-			return "Ocurrio un error al subir el video.";
-		}
-
-}
+        if ($exito == 1) {
+            return "Gracias";
+        } else {
+            return "Ocurrio un error al subir el video.";
+        }
+    }
 
 }
--- resources/views/upload/uploadvideo.blade.php
+++ resources/views/upload/uploadvideo.blade.php
@@ -24,21 +24,10 @@
     </div>
   </nav>
 
-  @if (count($errors) > 0)
-  <div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-  </div>
-  @endif
+ 
 
   <div class="row">
     <form method="POST"  class="form-signin" action="{{url('savevideo')}}" accept-charset="UTF-8" enctype="multipart/form-data">
-  {!! BootForm::open() !!}
-
-
       <br>
       <br>
       <div class="col-md-6 col-md-offset-3">
@@ -51,30 +40,24 @@
         </div>
 
         <div class="col-md-6 col-md-offset-3">
-
-          {!! BootForm::textarea('Cuentanos tus logros, metas y demás gracias a los cursos de México X', 'inputText') !!}
-
-          <!-- <label for="inputText">Cuentanos tus logros, metas y demás gracias a los cursos de México X</label>
-          <textarea class="form-control" id="inputText" rows="3" name="inputText" required></textarea> -->
+          <label for="inputText">Cuentanos tus logros, metas y demás gracias a los cursos de México X</label>
+          <textarea class="form-control" id="inputText" rows="3" name="inputText" required></textarea> 
         </div>
 
         <div class="col-md-6 col-md-offset-3">
-          <!-- <label for="inputVideo">Archivo de video</label>
-          <input type="file" id="inputVideo" name="inputVideo" required> -->
-          {!! BootForm::file('Archivo de video', 'inputVideo') !!}
+           <label for="inputVideo">Archivo de video</label>
+          <input type="file" id="inputVideo" name="inputVideo" required> 
+          
           <p class="help-block">MP4, MPG, 3GP ó WMV de máximo 20 MB</p>
         </div>
 
         <div class="checkbox col-md-6 col-md-offset-3">
-            {!! BootForm::checkbox('Acepto los términos y condiciones', 'checkbox') !!}
-            <!-- <label>
+          <label>
             <input type="checkbox" required>Acepto los términos y condiciones
-          </label> -->
+          </label> 
         </div>
 
         <button type="submit" class="btn btn-defaultcol-md-6 col-md-offset-3 btn btn-danger">Enviar</button>
-
-        {!! BootForm::close() !!}
       </form>
 
     </div>
