bd1085c760c0e81e854fea5a3c03869b3ded3fcb
jorgeomarmh <jorgeomarmh@gmail.com>
2016-08-10 15:23:38 -0500

    Plantilla correo mensual cursos con texto v10

--- resources/views/emails/masivo.blade.php
+++ resources/views/emails/masivo.blade.php
@@ -26,23 +26,23 @@
 
                 <table>
                   <tr>
-                    <td>
+                    <td style="padding: 5px">
                       <a href="http://mx.televisioneducativa.gob.mx/courses/IIIDx/IIIDRn103x/2016_s2/about" target="_blank"><img src="{{asset('/cursos_mensuales/1.png')}}"></a>
                     </td>
-                    <td>
+                    <td style="padding: 5px">
                       <a href="http://mx.televisioneducativa.gob.mx/courses/INEA/LPTMx/2016_S2/about" target="_blank"><img src="{{asset('/cursos_mensuales/2.png')}}"></a>
                     </td>
                   </tr>
                   <tr>
-                    <td>
+                    <td style="padding: 5px">
                       <a href="http://mx.televisioneducativa.gob.mx/courses/UPN/VE_UPN001_2/2016_S2/about" target="_blank"><img src="{{asset('/cursos_mensuales/3.png')}}"></a>
                     </td>
-                    <td>
+                    <td style="padding: 5px">
                       <a href="http://mx.televisioneducativa.gob.mx/courses/FEM-UAE/CBx/2016_T1/about" target="_blank"><img src="{{asset('/cursos_mensuales/4.png')}}"></a>
                     </td>
                   </tr>
                   <tr>
-                    <td>
+                    <td style="padding: 5px">
                       <a href="http://mx.televisioneducativa.gob.mx/courses/INEA/PPEH/2016E1/about" target="_blank"><img src="{{asset('/cursos_mensuales/5.png')}}"></a>
                     </td>
                     <td>
@@ -52,11 +52,8 @@
                 </table>
 
                 <br><br>
-                <p class="h5">
-                  <br>
-                  Refuerza tus competencias laborales y conocimientos en esta comunidad de aprendizaje.
-                </p>
-
+                  <h5>Refuerza tus competencias laborales y conocimientos en esta comunidad de aprendizaje.</h5>
+                <br>
             </div>
             <img src='http://mexicox.gob.mx:81/reportesmx/public/imagenes/email/linea.jpg'>
         </div>
