18dbab36bda3d83946014389280df9aece19e8bb
jorgeomarmh <jorgeomarmh@gmail.com>
2016-08-10 12:46:33 -0500

    Plantilla correo mensual cursos new tab v3

--- resources/views/emails/masivo.blade.php
+++ resources/views/emails/masivo.blade.php
@@ -18,20 +18,29 @@
                 <div class="col-md-12">
                   <div class="col-md-4 centered">
                     <a href="http://mx.televisioneducativa.gob.mx/courses/IIIDx/IIIDRn103x/2016_s2/about" target="_blank"><img src="{{asset('/cursos_mensuales/1.png')}}"></a>
+                    <br>
                   </div>
                   <div class="col-md-4 centered">
                     <a href="http://mx.televisioneducativa.gob.mx/courses/INEA/LPTMx/2016_S2/about" target="_blank"><img src="{{asset('/cursos_mensuales/2.png')}}"></a>
+                    <br>
                   </div>
                   <div class="col-md-4 centered">
                     <a href="http://mx.televisioneducativa.gob.mx/courses/UPN/VE_UPN001_2/2016_S2/about" target="_blank"><img src="{{asset('/cursos_mensuales/3.png')}}"></a>
-                  </div><br><br>
+                    <br>
+                  </div>
+                  <div class="col-md-12">
+                    <br>
+                  </div>
                   <div class="col-md-4 centered">
                     <a href="http://mx.televisioneducativa.gob.mx/courses/FEM-UAE/CBx/2016_T1/about" target="_blank"><img src="{{asset('/cursos_mensuales/4.png')}}"></a>
+                    <br>
                   </div>
                   <div class="col-md-4 centered">
                     <a href="http://mx.televisioneducativa.gob.mx/courses/INEA/PPEH/2016E1/about" target="_blank"><img src="{{asset('/cursos_mensuales/5.png')}}"></a>
+                    <br>
                   </div>
                   <div class="col-md-4 centered">
+                    <br>
                   </div>
                 </div>
 
