d4847a5567f5960495f8c795425d02599bb19bfe
jorgeomarmh <jorgeomarmh@gmail.com>
2016-08-10 15:39:26 -0500

    Plantilla correo mensual cursos con texto v12

--- resources/views/emails/masivo.blade.php
+++ resources/views/emails/masivo.blade.php
@@ -24,25 +24,25 @@
                   Inscríbete en los cursos gratuitos de MéxicoX que comienzan en agosto.
                 </p>
 
-                <table>
+                <table style="max-width: 600px;">
                   <tr>
-                    <td style="padding: 10px">
+                    <td style="padding: 10px; text-align: center;">
                       <a href="http://mx.televisioneducativa.gob.mx/courses/IIIDx/IIIDRn103x/2016_s2/about" target="_blank"><img src="{{asset('/cursos_mensuales/1.png')}}"></a>
                     </td>
-                    <td style="padding: 10px">
+                    <td style="padding: 10px; text-align: center;">
                       <a href="http://mx.televisioneducativa.gob.mx/courses/INEA/LPTMx/2016_S2/about" target="_blank"><img src="{{asset('/cursos_mensuales/2.png')}}"></a>
                     </td>
                   </tr>
                   <tr>
-                    <td style="padding: 10px">
+                    <td style="padding: 10px; text-align: center;">
                       <a href="http://mx.televisioneducativa.gob.mx/courses/UPN/VE_UPN001_2/2016_S2/about" target="_blank"><img src="{{asset('/cursos_mensuales/3.png')}}"></a>
                     </td>
-                    <td style="padding: 10px">
+                    <td style="padding: 10px; text-align: center;">
                       <a href="http://mx.televisioneducativa.gob.mx/courses/FEM-UAE/CBx/2016_T1/about" target="_blank"><img src="{{asset('/cursos_mensuales/4.png')}}"></a>
                     </td>
                   </tr>
                   <tr>
-                    <td style="padding: 10px">
+                    <td style="padding: 10px; text-align: center;">
                       <a href="http://mx.televisioneducativa.gob.mx/courses/INEA/PPEH/2016E1/about" target="_blank"><img src="{{asset('/cursos_mensuales/5.png')}}"></a>
                     </td>
                     <td>
