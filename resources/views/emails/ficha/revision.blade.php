ede28333175ab51f1ef78ef98e5bb13e3f90547c
Israel Toledo <j.israel.toledo@gmail.com>
2017-02-24 11:53:38 -0600

    Se corrigió error de parentesis

--- resources/views/emails/ficha/revision.blade.php
+++ resources/views/emails/ficha/revision.blade.php
@@ -9,7 +9,7 @@
         <a href="http://reportes.mexicox.gob.mx/formatos/ficha_tecnica/{{$ficha->id}}">
             <button> Revisar la ficha </button>
         </a>      
-        @if(!empty $ficha->aprobo())        
+        @if(!empty ($ficha->aprobo()))        
             Ficha aprobada por: {{$ficha->aprobo()->name}}
         @endif
     </body>
