4c6cfaaa5a490f521d6d3cbf40ead378259c0420
Israel Toledo <j.israel.toledo@gmail.com>
2017-03-27 17:10:29 -0600

    pruebas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,7 +32,7 @@ Route::get ('google_api/oauth2callback', function (Request $request){
         Session::put ('access_token', $client->getAccessToken());        
         $path = url('formatos/ficha_tecnica/publica/'.Session::get('id_ficha'));
         dd($path);
-        return Redirect::to($path);
+//        return Redirect::to($path);
     } else {        
         return Redirect::to($client->createAuthUrl());       
     }
