e03ea4cba8a5a0a870e219db47fd6464c9b3dcfe
Israel Toledo <j.israel.toledo@gmail.com>
2017-05-23 16:26:14 -0500

    Eliminando comentarios

--- app/Http/Middleware/AllowedBrowsersMiddleware.php
+++ app/Http/Middleware/AllowedBrowsersMiddleware.php
@@ -16,7 +16,6 @@ class AllowedBrowsersMiddleware
      */
     public function handle($request, Closure $next)
     {
-        //$browser = \hisorange\BrowserDetect\Facade\Parser::browserFamily();
         $agent = new Jenssegers\Agent\Agent;
         $browser = $agent->browser();
         Log::info('Browser detected: '.$browser);
