fa5286e7066eb7a093e4ba5651a083e244144029
Israel Toledo <j.israel.toledo@gmail.com>
2017-06-19 18:13:13 -0500

    Creando módulo de personal

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use App\Helpers\GoogleApi;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Session;
-
+use App\User;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -16,10 +16,23 @@ use Illuminate\Support\Facades\Session;
 |
 */
 
-/*
- * Ruta callback a donde regresa después de hacer el login en google para
- * usar las APIS.
- */
+
+Route::any ('instituciones/personal', function (){
+    $institucion_id = Input::get ('institucion_id');
+    if (empty($institucion_id)){
+        $personal = User::whereNull ('institucion_id')->get();
+    }else{
+        $personal = User::where('institucion_id', $institucion_id)->get();
+    }
+    
+    $instituciones = App\Model\Institucion::all()->pluck ('siglas', 'id');
+    
+    return view ('instituciones.personal')
+            ->with('personal', $personal)
+            ->with ('instituciones', $instituciones)
+            ->with ('institucion', App\Model\Institucion::find ($institucion_id));
+});
+
 
 Route::get ('recomendacion', function (){
     return view('formatos.ficha.recomendarNavegador')
@@ -99,6 +112,11 @@ Route::post ('usuario/asocia/institucion', function (){
         abort (404, 'Página no encontrada');
     }
 });
+
+/*
+ * Ruta callback a donde regresa después de hacer el login en google para
+ * usar las APIS.
+ */
 Route::get ('google_api/oauth2callback', function (Request $request){
     $client = new Google_Client();
     $client->setAuthConfigFile(config_path() . '/client_secret.json');
---
+++ resources/views/instituciones/personal.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.clean')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-6 col-md-offset-3">
+        <h1> Personal por institución </h1>
+    </div>    
+</div>
+
+<div class="row">
+    <div class="col-md-6 col-md-offset-3">        
+        <form action="{{url('instituciones/personal')}}" method="POST" class="form-inline">
+            {{csrf_field()}}
+            <label for="email"> Institución </label>
+            {!!Form::select ('institucion_id', $instituciones, null, ['class' => 'form-control'])!!}
+            <input type="submit" value="Mostrar personal" class="btn btn-info">
+        </form>                        
+    </div>            
+</div>
+<hr>
+<div class="row">
+    <div class="col-md-6 col-md-offset-3"> 
+        <h>
+            @if ()
+        </h>
+        <table class='table table-bordered'>
+            <thead>
+            <th> Nombre </th><th> Correo</th>
+            </thead>
+        @foreach ($personal as $persona)
+            <tr>
+                <td>{{$persona->name }}</td> <td>{{$persona->email}}</td>
+            </tr>
+        @endforeach 
+        </table>
+    </div>                   
+</div>
+@endsection
